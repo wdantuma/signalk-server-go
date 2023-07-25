508e752dea0829b19b254e7aab2225df0390357a
Willem Dantuma <willem.dantuma@dobs.nl>
2023-07-25 14:17:15 +0200

    Add parameters

--- cmd/s57-tiler/main.go
+++ cmd/s57-tiler/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 
@@ -9,20 +10,31 @@ import (
 )
 
 func main() {
-	datasets, err := dataset.GetS57Datasets("../../charts")
+
+	outputPath := flag.String("out", "./static/charts", "Output directory for vector tiles")
+	inputPath := flag.String("in", "./charts", "Input path S-57 maps")
+	minzoom := flag.Int("minzoom", 14, "Min zoom")
+	maxzoom := flag.Int("maxzoom", 14, "Max zoom")
+	flag.Parse()
+
+	datasets, err := dataset.GetS57Datasets(*inputPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	tiler := s57.NewS57Tiler(datasets)
-	tiles := tiler.GetTiles(datasets[0], 14)
 
-	total := len(tiles)
-	n := 0
-	for k := range tiles {
-		tiler.GenerateTile("../../static/charts", datasets[0], tiles[k])
-		done := float64(n) / float64(total) * 100
-		fmt.Printf("\rProcessed: %.0f %%    ", done)
-		n++
+	for z := *minzoom; z <= *maxzoom; z++ {
+		tiles := tiler.GetTiles(datasets[0], z)
+
+		total := len(tiles)
+		n := 0
+		for k := range tiles {
+			tiler.GenerateTile(*outputPath, datasets[0], tiles[k])
+			done := float64(n) / float64(total) * 100
+			fmt.Printf("\rZoom: %d, Processed: %.0f %%    ", z, done)
+			n++
+		}
 	}
+
 }
